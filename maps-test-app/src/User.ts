b9d40fc4f29e888bc8f74cbabc357fa3c32ada8c
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-19 21:19:16 +0100

    Continuing with samples.

---
+++ maps-test-app/src/User.ts
@@ -0,0 +1,18 @@
+// Users have a name and location (coords) - data  created using 'faker'
+import faker from 'faker';
+
+class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+}
