3627123e8c5b7e3c915e145ffcf84ca6f33d441d
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-23 17:34:01 +0100

    Type guard approach - will be ultimately changed though.

--- sorting-algorithm/src/index.ts
+++ sorting-algorithm/src/index.ts
@@ -1,16 +1,26 @@
 class Sorter {
-    constructor(public collection: number[]) {}
+    constructor(public collection: number[] | string) {}
 
     sort(): void {
         const { length } = this.collection;
 
+        // Compare/swap values (simple/not the best solution - illustration only)
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - 1; j++) {
-                // Compare/swap values (simple/not the best solution - illustration only)
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHandValue = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHandValue;
+                // Less than ideal code for starters - not a great solution...
+
+                // Type guard = dealing with arrays
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHandValue = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHandValue;
+                    }
+                }
+
+                // Type guard = dealing with a string
+                if (typeof this.collection === 'string') {
+                    
                 }
             }
         }
