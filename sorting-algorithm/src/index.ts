6b054d95d9cc4f488a6138df4466b937092e2477
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-23 19:40:08 +0100

    Testing the sort of a LinkedList

--- sorting-algorithm/src/index.ts
+++ sorting-algorithm/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // Configure a NumbersCollection/Sorter for use
 console.log("Handling the sort of a 'NumbersCollection'...");
@@ -9,18 +10,38 @@ let sorter = new Sorter(numbersCollection);
 
 // Pre-sort
 console.log(`Before sort: ${ numbersCollection.data }`);
-sorter.sort();
 
 // Post-sort
+sorter.sort();
 console.log(`After sort: ${ numbersCollection.data }`);
 
 // Second use of Sorter - CharactersCollection
 console.log("Handling the sort of a 'CharactersCollection'...");
-const charactersCollection = new CharactersCollection('zyXWvUtsrQpOnmlkJihgfEdCBa');
+const charactersCollection = new CharactersCollection('zyXWvgitUtsrQpOnmlkJihgfEdCBa');
+sorter = new Sorter(charactersCollection);
 
 // Pre-sort
 console.log(charactersCollection.data);
-sorter = new Sorter(charactersCollection);
 
 // Post-sort
 sorter.sort();
+console.log(charactersCollection.data);
+
+// LinkedList tests
+console.log("Handling the sort of a 'LinkedList'...");
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+sorter = new Sorter(linkedList);
+
+// Pre-sort
+console.log('Before sort...');
+linkedList.print();
+
+// Post-sort
+sorter.sort();
+console.log('After sort...');
+linkedList.print();
