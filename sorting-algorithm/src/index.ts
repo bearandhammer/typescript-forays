a59f3fecccced4706080a84602c94851b3e17884
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-23 18:55:40 +0100

    Inclusion of the CharactersCollection type and implementation.

--- sorting-algorithm/src/index.ts
+++ sorting-algorithm/src/index.ts
@@ -1,9 +1,11 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 // Configure a NumbersCollection/Sorter for use
+console.log("Handling the sort of a 'NumbersCollection'...");
 const numbersCollection = new NumbersCollection([14541, 12, -7, -1, 1, 10]);
-const sorter = new Sorter(numbersCollection);
+let sorter = new Sorter(numbersCollection);
 
 // Pre-sort
 console.log(`Before sort: ${ numbersCollection.data }`);
@@ -11,3 +13,14 @@ sorter.sort();
 
 // Post-sort
 console.log(`After sort: ${ numbersCollection.data }`);
+
+// Second use of Sorter - CharactersCollection
+console.log("Handling the sort of a 'CharactersCollection'...");
+const charactersCollection = new CharactersCollection('zyXWvUtsrQpOnmlkJihgfEdCBa');
+
+// Pre-sort
+console.log(charactersCollection.data);
+sorter = new Sorter(charactersCollection);
+
+// Post-sort
+sorter.sort();
