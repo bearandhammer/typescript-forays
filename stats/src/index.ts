23445d843b4f175577d74adb2764b68641aac0ec
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-25 12:55:39 +0100

    First pieces of data manipulation prior to reporting

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,13 @@
 import fs from 'fs';
 
 const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'           // String representing content
+    // String representing content
+    encoding: 'utf-8'
+})
+.split('\n')
+//.map(match => match.split(','));
+.map((match: string): string[] => {
+    return match.split(',');
 });
 
 console.log(matches);
