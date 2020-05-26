95c66e0326ceef61bae394699b0f6f71a1bbaa27
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-26 16:31:58 +0100

    Abstract class testing with generics.

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,14 +1,12 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-type MatchData = [ Date, string, string, number, number, MatchResult, string ];
-
-export class CsvFileReader {
-    data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
 
     constructor(public fileName: string) {
     }
+
+    abstract mapRow(row: string[]): T;
 
     read(): void {
         this.data = fs.readFileSync(this.fileName, {
@@ -16,20 +14,10 @@ export class CsvFileReader {
             encoding: 'utf-8'
         })
         .split('\n')
-        .map((match: string): string[] => {
-            return match.split(',');
+        .map((row: string): string[] => {
+            return row.split(',');
         })
         // Convert to correct types
-        .map((row: string[]): MatchData => {
-            return [
-                dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5] as MatchResult,     // 'H', 'A', 'D' (type assertion),
-                row[6]
-            ];
-        });
+        .map(this.mapRow);
     }
 }
