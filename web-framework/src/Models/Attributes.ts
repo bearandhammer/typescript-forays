6f3358ac0f3a1268adae716ea2670b159cf7715f
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-28 15:57:12 +0100

    Implementation of save fleshed out.

--- web-framework/src/Models/Attributes.ts
+++ web-framework/src/Models/Attributes.ts
@@ -10,4 +10,8 @@ export class Attributes<T> {
     set(update: T): void {
         Object.assign(this.data, update);
     }
+
+    getAll(): T {
+        return this.data;
+    }
 }
--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './Models/User';
 
-const user = new User({ id: 2 });
+const user = new User({ id: 2, name: 'Newer Name', age: 1 });
 
-user.on('change', () => {
+user.on('save', () => {
     console.log(user);
 });
 
-user.fetch();
+user.save();
