3671fd69a7f2ddd579c90bdd25c8f0c83e9b702d
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-28 13:31:36 +0100

    First implementation of adding Attributes to User.

--- web-framework/src/Models/User.ts
+++ web-framework/src/Models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 // ? = optional property
 export interface UserProps {
@@ -13,4 +14,9 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
     public events: Eventing = new Eventing();
     public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+    public attributes: Attributes<UserProps>;
+
+    constructor(attrs: UserProps) {
+        this.attributes = new Attributes<UserProps>(attrs);
+    }
 }
