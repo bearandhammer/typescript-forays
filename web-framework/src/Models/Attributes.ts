d974a00ab52ad6ce4a7b6fec31672613487f49ad
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-28 14:15:46 +0100

    Adding pass through members.
    In addition, dealt with some context issues.

--- web-framework/src/Models/Attributes.ts
+++ web-framework/src/Models/Attributes.ts
@@ -3,7 +3,7 @@ export class Attributes<T> {
     }
 
     // Type of K can only be one of the 'keys' of T. Return value = type T value of key K
-    get<K extends keyof T>(key: K): T[K] {
+    get = <K extends keyof T>(key: K): T[K] => {
         return this.data[key];
     }
 
--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,3 +1,10 @@
 import { User } from './Models/User';
 
-// TODO...further implementation
+const user = new User({ name: 'Billy Zane', age: 58 });
+console.log(user.get('name'));
+
+user.on('change', () => {
+    console.log('User was changed!');
+});
+
+user.trigger('change');
