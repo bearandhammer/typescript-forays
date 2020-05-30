3d4a956060aca7221aeec189179d4b1f2cbf1aa3
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-30 11:50:07 +0100

    Implementation of authenticated routes.
    Due to pulled apart when decorators are brought into the fold.

--- express-sample/src/index.ts
+++ express-sample/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 // Create an express application and attach route
 const app = express();
 
-// Add in middleware (for form parsing) -> express 'Request' does not inherently have a 'body' property, this is discovered/added by the middleware
+// Add in middleware (for form parsing/sessions) -> express 'Request' does not inherently have a 'body' property, this is discovered/added by the middleware
 app.use(bodyParser.urlencoded({
     extended: true 
+}));
+app.use(cookieSession({
+    keys: ['somekey']    
 }));
 
 // Configure routes from our preconfigured router object
--- express-sample/src/routes/loginRoutes.ts
+++ express-sample/src/routes/loginRoutes.ts
@@ -1,7 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface BodyRequest extends Request {
     body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+
+    // User does not have a session or is not logged in
+    res.status(403);
+    res.send('Not permitted.');
 }
 
 const router = Router();
@@ -25,12 +36,43 @@ router.post('/login', (req: BodyRequest, res: Response) => {
     // Parser middleware required to handle form content - preconfigured
     const { email, password } = req.body;
 
-    if (email) {
-        res.send(email.toUpperCase());   
+    if (email && password && email === 'admin@test.com' && password === "hithere") {
+        if (req.session) {
+            // Hmmm...TODO - refine
+            req.session.loggedIn = true;
+            res.redirect('/');
+        }
+    } else {
+        res.send('Invalid email or password.');
     }
-    else {
-        res.send('Error: email must be defined.');
+});
+
+router.get('/', (req: Request, res: Response) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+            <div>
+                <div>You are logged in</div>
+                <a href="/protected">Go to protected route</a>
+                <a href="/logout">Logout</a>
+            </div>
+        `);
+    } else {
+        res.send(`
+            <div>
+                <div>You are not logged in</div>
+                <a href="/login">Login</a>
+            </div>
+        `);
     }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = null;
+    res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('Welcome to protected route, logged in user.');
 });
 
 export { router };
