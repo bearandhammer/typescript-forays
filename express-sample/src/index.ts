043bea132823e382d99940bbf559c246906f2b87
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-05-30 09:42:16 +0100

    Most basic of form submissions.

--- express-sample/src/index.ts
+++ express-sample/src/index.ts
@@ -1,15 +1,17 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 // Create an express application and attach route
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`
-        <div>
-            <h1>Hi there</h1>
-        </div>
-    `);
-});
+// Add in middleware (for form parsing)
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
+
+// Configure routes from our preconfigured router object
+app.use(router);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
