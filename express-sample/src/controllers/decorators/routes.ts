97625de809ac133251c42a05c583e683f0b2c74a
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-02 09:46:45 +0100

    Bug fix for path metadata.
    Path 'value' was not being passed into the defineMetadata function (a big issue!).

--- express-sample/src/controllers/decorators/routes.ts
+++ express-sample/src/controllers/decorators/routes.ts
@@ -6,7 +6,7 @@ import { MetadataKeys } from './MetadataKeys';
 function routeBinder(method: string) {
     return function (path: string) {
         return function(target: any, key: string, desc: PropertyDescriptor) {
-            Reflect.defineMetadata(MetadataKeys.Path, target, key);
+            Reflect.defineMetadata(MetadataKeys.Path, path, target, key);
             Reflect.defineMetadata(MetadataKeys.Method, method, target, key);
         }
     }
