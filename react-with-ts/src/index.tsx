74e2176cc7d047caa4e2d21c6c1080c496f9d34a
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-02 16:23:00 +0100

    Redux setup.

--- react-with-ts/src/index.tsx
+++ react-with-ts/src/index.tsx
@@ -1,48 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers  } from './reducers';
 
-interface AppProps {
-    colour: string
-}
-
-// Functional components
-const App = (props: AppProps): JSX.Element => {
-    return <div>{ props.colour }</div>
-}
-
-// interface AppState {
-//     counter: number
-// }
-
-// class App extends React.Component<AppProps, AppState> {
-//     constructor(props: AppProps) {
-//         super(props);
-//         this.state = { counter: 0 };
-//     }   
-
-//     // Arrow functions - want to make sure these are bound (no 'contextual' issues in play!)
-//     onIncrement = () => {
-//         this.setState({ counter: this.state.counter + 1 });
-//     }
-
-//     onDecrement = () => {
-//         this.setState({ counter: this.state.counter - 1 });
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={ this.onIncrement }>Increment</button>
-//                 <button onClick={ this.onDecrement }>Decrement</button>
-//                 { this.state.counter }
-//             </div>
-//         )
-//     }
-// }
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App colour="red" />
-    </React.StrictMode>,
+    <Provider store={ store }>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
---
+++ react-with-ts/src/reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+// Dummy reducer...
+export const reducers = combineReducers({
+    counter: () => 1
+});
