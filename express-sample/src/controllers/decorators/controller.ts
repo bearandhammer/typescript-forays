2797b4bc9a978568f7e391220a540bf4961cc171
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-02 09:49:53 +0100

    Middleware test added (for future ref).
    Will be removed shortly, but I'll keep the reference in the history on the repo nonetheless.

--- express-sample/src/controllers/decorators/controller.ts
+++ express-sample/src/controllers/decorators/controller.ts
@@ -12,10 +12,10 @@ export function controller(routePrefix: string) {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata(MetadataKeys.Path, target.prototype, key);
             const method: Methods = Reflect.getMetadata(MetadataKeys.Method, target.prototype, key);
-            const middlewares = Reflect.getMetadata(MetadataKeys.Middleware, target, key);
+            const middlewares = Reflect.getMetadata(MetadataKeys.Middleware, target.prototype, key) || [];
 
             if (path) {
-                router[method](`${ routePrefix }${ path }`, routeHandler);
+                router[method](`${ routePrefix }${ path }`, ...middlewares, routeHandler);
             }
         }
     }
--- express-sample/src/controllers/decorators/use.ts
+++ express-sample/src/controllers/decorators/use.ts
@@ -6,8 +6,6 @@ export function use(middleware: RequestHandler) {
     return function(target: any, key: string, desc: PropertyDescriptor) {
         const middlewares = Reflect.getMetadata(MetadataKeys.Middleware, target, key) || [];
 
-        middlewares.push(middleware);
-
-        Reflect.defineMetadata(MetadataKeys.Middleware, middlewares, target, key);
+        Reflect.defineMetadata(MetadataKeys.Middleware, [...middlewares, middleware], target, key);
     }
 }
